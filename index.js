61d40c1d1885dfc64b417da98a957d39d4a2038e
daniel123tralgir <46651503+daniel123tralgir@users.noreply.github.com>
2019-01-13 17:57:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
+bot.on('ready', () => {
+    // Set bot status to: "Playing with JavaScript"
+    bot.user.setActivity("Pornos",{type: "WATCHING"} )
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
 
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    // client.user.setActivity("TV", {type: "WATCHING"})
+})
+
+
+bot.on('message', msg => {
+  if (msg.content === 'mach was Sinnvolles, DEUTSCHLAND LEBT') {
+    msg.replx('ok');
+    bot.user.setActivity("Katzenvideos",{type: "WATCHING"} )
+  }
+
+  if (msg.content === 'Spiel mal was, DEUTSCHLAND LEBT') {
+    bot.user.setActivity("mit sich selbst rum",{type: "PLAYING"} )
+
+    msg.reply('hdf');
+  }
+
+  if (msg.content === 'hdf') {
+    bot.user.setActivity("lol",{type: "WATCHING"} )
+
+    msg.reply('lol');
+  }
+
+
+ }
+
+
+
+   
+
+);
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
