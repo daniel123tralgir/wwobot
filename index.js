91892d72914218d05e701ff3f9fd104f0726f3b5
daniel123tralgir <46651503+daniel123tralgir@users.noreply.github.com>
2019-01-13 17:00:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('ready', () => {
+    // Set bot status to: "Playing with JavaScript"
+    bot.user.setActivity("Pornos",{type: "WATCHING"} )
+
+
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    // client.user.setActivity("TV", {type: "WATCHING"})
+})
+
+
+bot.on('message', msg => {
+  if (msg.content === 'mach was Sinnvolles, DEUTSCHLAND LEBT') {
+    msg.replx('ok');
+    bot.user.setActivity("Katzenvideos",{type: "WATCHING"} )
+  }
+
+  if (msg.content === 'Spiel mal was, DEUTSCHLAND LEBT') {
+    bot.user.setActivity("mit sich selbst rum",{type: "PLAYING"} )
+
+    msg.reply('hdf');
+  }
+
+  if (msg.content === 'hdf') {
+    bot.user.setActivity("lol",{type: "WATCHING"} )
+
+    msg.reply('lol');
+  }
+
+
+ }
+
+
+
+   
+
+);
+
+
+
+
+
+
+
+bot.login('NTMyMjcwNjU2MzIzMTkwNzg0.DxzJHA.kDT8DiMgcpX5M1_gcQkGfEf9bi8');
+
+
