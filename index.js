e546d359fde8c513ff556a8a51ecce503d5598fd
daniel123tralgir <46651503+daniel123tralgir@users.noreply.github.com>
2019-02-03 13:50:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const Welcome = require("./welcome.js");
 const bot = new Discord.Client();
+const firebase = require("firebase-admin");
 
+const serviceAccount = require("./serviceAccountKey.json");
 
 
 bot.on('ready', () => {
